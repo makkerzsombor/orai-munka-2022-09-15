89deec67e159287998b2e807d605ceef10221b51
Zsombor Makker <makker.zsombor@gmail.com>
2022-09-15 12:11:30 +0200

    atlag function

--- src/lista.js
+++ src/lista.js
@@ -6,3 +6,11 @@ function hozzaad(szam) {
         t.push(szam);
     }
 }
+
+function atlag(t) {
+    let ossz = 0;
+    for (let index = 0; index < t.length; index++) {        
+        ossz += t[i];
+    }
+    console.log("Az átlag: "+ossz/t-length);
+}
